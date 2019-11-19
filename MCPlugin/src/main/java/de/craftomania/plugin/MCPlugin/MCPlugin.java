c3940e79ea2e103bb27a888dcdceadade15c0fe2
maamdaad <57961915+maamdaad@users.noreply.github.com>
2019-11-19 20:12:38 +0100

    LALA

---
+++ MCPlugin/src/main/java/de/craftomania/plugin/MCPlugin/MCPlugin.java
@@ -0,0 +1,18 @@
+package de.craftomania.plugin.MCPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCPlugin extends JavaPlugin {
+
+	@Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+		System.out.println("LALALA");
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+	
+}
