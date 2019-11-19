cdb94fab35f78c8bbdf915dd56be813db3f604f6
maamdaad <57961915+maamdaad@users.noreply.github.com>
2019-11-19 21:02:49 +0100

    lala

--- MCPlugin/src/main/java/de/craftomania/plugin/MCPlugin/MCPlugin.java
+++ MCPlugin/src/main/java/de/craftomania/plugin/MCPlugin/MCPlugin.java
@@ -1,18 +1,48 @@
 package de.craftomania.plugin.MCPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCPlugin extends JavaPlugin {
 
 	@Override
     public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
-		System.out.println("LALALA");
+        getLogger().info("MCPlugin an!");
     }
-    
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("MCPlugin aus!");
+        
     }
 	
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("arena")) { 
+    		
+    		checkCommand(sender, cmd, label, args);
+    		
+    		return true;
+    	} 
+    	return false; 
+    }
+    
+    private void checkCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (args[0].equalsIgnoreCase("joinclass")) {
+    		String classname = args[1];
+    		String playername = args[2];
+    		
+    		Player target = Bukkit.getServer().getPlayer(playername);
+    		
+    		joinClass(target, classname);
+    		
+    		
+    	}
+    }
+    
+    private void joinClass(Player target, String classname) {
+    	
+    }
+    
 }
